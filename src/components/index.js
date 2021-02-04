8172cd311ad71b833b281017a450ee5ae6a10fd0
lujing <lujing@spap.com>
2021-02-04 14:38:31 +0800

    更新

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 import Render from './Render/Render.vue'
 import One from './template/One.vue'
+import Six from './template/Six.vue'
 export {
   Render,
-  One
+  One,
+  Six
 }
---
+++ src/components/template/Six.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="data.isPc ?'':'isMobile'">
+    <div class="pc-show">
+      PC新闻模板
+    </div>
+    <div class="mobile-show">
+      <div class="test">h5新闻模板</div>
+    </div>
+  </div>
+</template>
+<script>
+import mixin from './comm/mixins/index'
+export default {
+  mixins: [mixin],
+  data() {
+    return {
+      data: {
+        isPc: true,
+        tempData: {}
+      }
+    }
+  },
+  mounted() {
+    console.log(this.data, '======-----')
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .mobile-show {
+    display: none;
+  }
+  .isMobile{
+    .pc-show {
+      display: none;
+    }
+    .mobile-show {
+      display: block;
+      font-size: 0.18rem;
+    }
+  }
+</style>
